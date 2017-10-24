4186374b8a9dcac8ce524666154597367b278972
tina 0420 <hsu.tina88@yagoo.com.tw>
2017-10-24 16:36:38 +0800

    算術運算子

--- src/com/tina/Hello.java
+++ src/com/tina/Hello.java
@@ -1,15 +1,37 @@
 package com.tina;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Hello world");
+        System.out.println("請輸入您的名字:");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println("Hello," +line);
         
-        person p = new person();
-        p.sayHello();
+        System.out.println("你幾歲啊?");
+        line = scanner.nextLine();
+        int age = Integer.parseInt(line);
+        System.out.println(age>25);
         
+        int x = 5;
+        int y = 8;
+        int z = x%y;
+        System.out.println(z);
         
+        System.out.println(x == y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x > y);
+        
+        System.out.println(x >> 1);
+        x++;
+        x--;
+        System.out.println(x);
+        
+
 	}
 
 }
