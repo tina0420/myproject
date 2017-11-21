5b6fae9d94a4da50ebc6702709ef3c2c763d929a
tina0420 <hsu.tina88@yahoo.com.tw>
2017-11-21 19:30:34 +0800

    practice

--- src/com/tina/Practice.java
+++ src/com/tina/Practice.java
@@ -11,6 +11,53 @@ public class Practice {
 				}                                             // \t是齊頭:tab鍵
 				System.out.println();
 			}
+		
+		System.out.println();
+		
+		int score = 95;
+		int level = score/10;
+		switch(level) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				System.out.println("Failed");
+				break;
+			case 6:
+				System.out.println("Pass");
+				break;
+			case 7:
+				System.out.println("Good");
+			    break;
+			case 8:
+				System.out.println("Great");
+			    break;
+			case 9:
+				System.out.println("Excellent!");
+				
+				System.out.println();
+				
+				Scanner scanner = new Scanner(System.in);
+				String line = scanner.nextLine();
+				int n = Integer.parseInt(line);
+				for (int i = 1; i<=n; i++) {
+					if(i % 3 == 0) {
+						System.out.print(i+"*");
+						continue;
+					}
+					System.out.print(i +" ");
+				}
+				
+				
+		}
+				
+		
+		
+		
+		
+		
 		}
 
 	}
