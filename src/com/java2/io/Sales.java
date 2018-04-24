3bcfd60a15dad2334504834261ddca427c87ac9e
tina0420 <hsu.tina88@yahoo.com.tw>
2018-04-24 11:25:26 +0800

    sales report

---
+++ src/com/java2/io/Sales.java
@@ -0,0 +1,23 @@
+package com.java2.io;
+
+public class Sales {
+	int type;
+	int amount;
+	public Sales(int type,int amount) {
+		this.type=type;
+		this.amount=amount;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type=type;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount=amount;
+	}
+
+}
--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -1,10 +1,21 @@
 package com.java2.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.text.html.ListView;
+
+import com.tina.sogo.Customer;
+import com.tina.sogo.GoldenCustomer;
+import com.tina.sogo.SilverCustomer;
 
 public class Sogo {
 	Scanner scanner = new Scanner(System.in);
@@ -32,6 +43,50 @@ public class Sogo {
 			case 1:
 				inputSales();
 			    break;
+			case 2:
+				List<Sales> list = new ArrayList<>();
+				try {
+					FileInputStream fis = new FileInputStream("sales.txt");
+					InputStreamReader isr = new InputStreamReader(fis);
+					BufferedReader in = new BufferedReader(isr);
+					String line = in.readLine();
+					while(line != null) {
+						try {
+							String[] token = line.split("\t");
+							int type = Integer.parseInt(token[0]);
+							int amount = Integer.parseInt(token[1]);
+							Sales sales = new Sales(type, amount);
+							list.add(sales);
+						}catch(NumberFormatException e) {
+							System.out.println("資料格式錯誤");
+							return;
+						}
+						line = in.readLine();
+					}
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			for (Sales sales: list) {
+				Customer customer = null;
+				switch(sales.type) {
+				case 1:
+					customer = new Customer(sales.getAmount());
+					break;
+				case 2:
+					customer = new SilverCustomer(sales.getAmount());
+					break;
+				case 3:
+					customer = new GoldenCustomer(sales.getAmount());
+					break;
+				}
+				customer.print();
+			}
+			break;
+			
 			case 3:
 				return;
 			}
