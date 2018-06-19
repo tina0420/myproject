3b2dd6d42de6260017de2f3cf937df998ab54e1a
tina0420 <hsu.tina88@yahoo.com.tw>
2018-06-19 14:16:30 +0800

    Vending,Drink,Analysis,Design

---
+++ src/com/java2/oo/Drink.java
@@ -0,0 +1,31 @@
+package com.java2.oo;
+
+public class Drink {
+	int id;
+	String name;
+	int price;
+	public void Drink(int id, String name, int price){
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public int getId(){
+		return id;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setPrice(int price){
+		this.price = price;
+	}
+	public int getPrice(){
+		return price;
+	}
+
+}
---
+++ src/com/java2/oo/VendingMain.java
@@ -0,0 +1,13 @@
+package com.java2.oo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMain {
+
+	public static void main(String[] args) {
+		List<Drink> drinks = new ArrayList<>();
+		
+	}
+
+}
